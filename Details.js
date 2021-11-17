c5f629cd0f39a0ebd07e86a423e8132f7d60f50b
Rohini-2 <74820598+Rohini-2@users.noreply.github.com>
2021-11-17 18:19:17 +0530

    Add files via upload

---
+++ Details.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View, StyleSheet, Button, Alert } from 'react-native';
+
+export default class DetailsScreen extends Component{
+render(){
+    return(
+        <View>
+            <Text>DetailsScreen</Text>
+        </View>
+    )
+}
+}
